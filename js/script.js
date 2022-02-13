caf71e0864d52ae3715a6e287bce15b018ea95bd
DasminX <88339375+DasminX@users.noreply.github.com>
2022-02-13 18:54:10 +0100

    Add files via upload
    I added colors feature, made it responsive on mobiles and cleaned code
    In next commit I will rewrite it to SCSS and do the last cleaning up, maybe some changes

--- js/script.js
+++ js/script.js
@@ -1,117 +1,144 @@
 "use strict";
 
-/* Modal variables */
-const openModalButton = document.querySelector(".modal-button");
-const closeModalButton = document.querySelector(".close-modal");
-const modalContainer = document.querySelector(".modal-container");
-const overlay = document.querySelector(".overlay");
+(() => {
+  /* Time variables */
+  let time = 0;
+  let timer;
+  let minutes;
+  let seconds;
 
-/* Starting timer variables */
-const playButton = document.querySelector(".play-button");
-const minutesEl = document.querySelector(".minutes");
-const secondsEl = document.querySelector(".seconds");
-const timeContainer = document.querySelector(".time-container");
+  /* Brush variables */
+  const brushButton = document.querySelector(".brush-button");
+  const colorsContainer = document.querySelector(".colors-container");
+  const colors = document.querySelectorAll(".color");
 
-let time = 0;
-let timer;
-let minutes;
-let seconds;
+  /* Modal variables */
+  const openModalButton = document.querySelector(".modal-button");
+  const closeModalButton = document.querySelector(".close-modal");
+  const modalContainer = document.querySelector(".modal-container");
+  const overlay = document.querySelector(".overlay");
 
-/* Pausing timer variables */
-const pauseButton = document.querySelector(".pause-button");
+  /* Starting timer variables */
+  const playButton = document.querySelector(".play-button");
+  const minutesEl = document.querySelector(".minutes");
+  const secondsEl = document.querySelector(".seconds");
+  const timeContainer = document.querySelector(".time-container");
 
-/* Stopping and saving timer's last score to archives' variables */
-const lastTime = document.querySelector(".last-time");
-const stopButton = document.querySelector(".stop-button");
+  /* Pausing timer variable */
+  const pauseButton = document.querySelector(".pause-button");
 
-let archivedNumber = 0;
+  /* Stopping timer and saving timer's recent score to archives' variables */
+  const recentTime = document.querySelector(".recent-time");
+  const stopButton = document.querySelector(".stop-button");
 
-/* Reset all variables */
-const resetButton = document.querySelector(".reset-button");
+  let archivedNumber = 0;
 
-/* Archive variables */
-const archiveButton = document.querySelector(".archive-button");
-const archivedEl = document.querySelector(".archived");
+  /* Reset button variable */
+  const resetButton = document.querySelector(".reset-button");
 
-///////////////////////////////////////////////////////////////
-/* Modal functions and listeners */
-const showModal = () => {
-  [modalContainer, overlay].forEach((el) => el.classList.remove("hidden"));
-};
+  /* Archive variables */
+  const archiveButton = document.querySelector(".archive-button");
+  const archivedEl = document.querySelector(".archived");
 
-const closeModal = () => {
-  [modalContainer, overlay].forEach((el) => el.classList.add("hidden"));
-};
-
-openModalButton.addEventListener("click", showModal);
-closeModalButton.addEventListener("click", closeModal);
-
-/* Starting timer functions and listeners */
-const runTimer = () => {
-  if (!timeContainer.classList.contains("timer-run")) {
-    timeContainer.classList.add("timer-run");
-    timer = setInterval(() => {
-      time++;
-      seconds = Math.floor(time % 60);
-      seconds = seconds < 10 ? `0${seconds}` : seconds;
-      minutes = Math.floor(time / 60);
-      minutesEl.textContent = minutes;
-      secondsEl.textContent = seconds;
-    }, 100);
-  }
-};
-
-playButton.addEventListener("click", runTimer);
-
-/* Pausing timer functions and listeners */
-const pauseTimer = () => {
-  clearInterval(timer);
-  timeContainer.classList.remove("timer-run");
-};
-
-pauseButton.addEventListener("click", pauseTimer);
-
-/* Stop and save functions and listeners*/
-
-const stopAndSaveTime = () => {
-  time = 0;
-  timeContainer.classList.remove("timer-run");
-  lastTime.textContent = `Ostatni czas: ${minutes}:${seconds}`;
-  minutesEl.textContent = "0";
-  secondsEl.textContent = "00";
-
-  archivedNumber++;
-  if (archivedNumber > 6) {
-    archivedNumber = 1;
-    archivedEl.innerHTML = ``;
-  }
-  clearInterval(timer);
-  archivedEl.insertAdjacentHTML(
-    "beforeend",
-    `        <p>Pomiar nr ${archivedNumber}: <span>${minutes}:${seconds}</span></p>
-  `
+  ///////////////////////////////////////////////////////////////
+  /* Brush container functions and listeners */
+  colors.forEach((col) =>
+    col.addEventListener("click", (e) => {
+      const colorCode = getComputedStyle(e.target).backgroundColor;
+      document.documentElement.style.setProperty("--primary-color", colorCode);
+    })
   );
-};
 
-stopButton.addEventListener("click", stopAndSaveTime);
+  const showColors = () => {
+    colorsContainer.classList.toggle("show-colors");
+  };
 
-/* Reset button functions and listeners */
-const resetAll = () => {
-  time = 0;
-  timeContainer.classList.remove("timer-run");
-  lastTime.textContent = ``;
-  minutesEl.textContent = "0";
-  secondsEl.textContent = "00";
-  archivedEl.innerHTML = ``;
-  archivedNumber = 0;
-};
+  brushButton.addEventListener("click", showColors);
 
-resetButton.addEventListener("click", resetAll);
+  /* Modal functions and listeners */
+  const toggleModal = () => {
+    [modalContainer, overlay].forEach((el) => el.classList.toggle("hidden"));
+  };
 
-/* Archive functions and listeners */
+  openModalButton.addEventListener("click", toggleModal);
+  closeModalButton.addEventListener("click", toggleModal);
 
-const toggleArchived = () => {
-  archivedEl.classList.toggle("hidden");
-};
+  /* Starting timer functions and listeners */
+  const runTimer = () => {
+    if (!timeContainer.classList.contains("timer-run")) {
+      timeContainer.classList.add("timer-run");
+      timer = setInterval(() => {
+        time++;
+        seconds = Math.floor(time % 60);
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
+        minutes = Math.floor(time / 60);
+        minutesEl.textContent = minutes;
+        secondsEl.textContent = seconds;
+      }, 1000);
+    }
+  };
 
-archiveButton.addEventListener("click", toggleArchived);
+  playButton.addEventListener("click", runTimer);
+
+  /* Pausing timer functions and listeners */
+  const pauseTimer = () => {
+    clearInterval(timer);
+    timeContainer.classList.remove("timer-run");
+  };
+
+  pauseButton.addEventListener("click", pauseTimer);
+
+  /* Helper function to keep DRY */
+  const timeResetter = () => {
+    time = 0;
+    minutesEl.textContent = "0";
+    secondsEl.textContent = "00";
+  };
+
+  /* Stopping and saving timer functions and listeners*/
+  const stopAndSaveTime = () => {
+    if (secondsEl.textContent === "00") return;
+
+    timeResetter();
+
+    recentTime.textContent = `Ostatni czas: ${minutes}:${seconds}`;
+
+    timeContainer.classList.remove("timer-run");
+
+    archivedNumber++;
+    if (archivedNumber > 6) {
+      archivedNumber = 1;
+      archivedEl.innerHTML = ``;
+    }
+    archivedEl.insertAdjacentHTML(
+      "beforeend",
+      `<p>Pomiar nr ${archivedNumber}: <span>${minutes}:${seconds}</span></p>
+  `
+    );
+
+    clearInterval(timer);
+  };
+
+  stopButton.addEventListener("click", stopAndSaveTime);
+
+  /* Reset button functions and listeners */
+  const resetAll = () => {
+    timeResetter();
+    archivedNumber = 0;
+    recentTime.textContent = ``;
+    archivedEl.innerHTML = ``;
+
+    timeContainer.classList.remove("timer-run");
+    clearInterval(timer);
+  };
+
+  resetButton.addEventListener("click", resetAll);
+
+  /* Archive functions and listeners */
+
+  const toggleArchived = () => {
+    archivedEl.classList.toggle("hidden");
+  };
+
+  archiveButton.addEventListener("click", toggleArchived);
+})();
